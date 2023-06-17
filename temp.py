8439ca5dad6d5903052523c05c174d7ffbaa057a
SaikoReyes <72665381+SaikoReyes@users.noreply.github.com>
2023-06-16 18:53:44 -0600

    Nuevo código para el termometro

--- temp.py
+++ temp.py
@@ -1,47 +1,32 @@
 import tkinter as tk
-import matplotlib.pyplot as plt
+from tkinter import ttk
+import serial
+import time
 
-def actualizar_grafica(temperatura):
-    # Establecer el rango de la barra o termómetro
-    min_valor = 0
-    max_valor = 255
+# Open the serial port
+ser = serial.Serial('/dev/cu.usbmodem1301', 9600)
 
-    # Normalizar el valor de la temperatura dentro del rango de 0 a 1
-    valor_normalizado = (temperatura - min_valor) / (max_valor - min_valor)
+def update_progress_bar():
+  # Read the value from the serial port
+  line = ser.readline()
+  value = int(line)
 
-    # Actualizar la barra o termómetro en la ventana
-    canvas.coords(barra, 10, 10, 10 + valor_normalizado * 400, 40)
+  # Update the progress bar
+  progress['value'] = value
+  print("hola")
+  print(value)
+  # Schedule the next update
+  root.after(1000, update_progress_bar)
 
-    # Actualizar la etiqueta de temperatura en la ventana
-    etiqueta_temperatura.config(text=f'Temperatura: {temperatura} / 255')
+# Create the main window
+root = tk.Tk()
 
-# Crear la ventana
-ventana = tk.Tk()
-ventana.title('Termómetro')
-ventana.geometry('500x100')
+# Create the progress bar
+progress = tk.ttk.Progressbar(root, length=400, mode='determinate', maximum=255)
+progress.pack()
 
-# Crear el lienzo para la barra o termómetro
-canvas = tk.Canvas(ventana, width=420, height=50, bg='white')
-canvas.pack()
+# Start the update loop
+root.after(1000, update_progress_bar)
 
-# Dibujar la barra inicial en el lienzo
-barra = canvas.create_rectangle(10, 10, 10, 40, fill='blue')
-
-# Crear la etiqueta "Temperatura"
-etiqueta_texto = tk.Label(ventana, text='Temperatura')
-etiqueta_texto.pack()
-
-# Crear la etiqueta de temperatura
-etiqueta_temperatura = tk.Label(ventana, text='0 / 255')
-etiqueta_temperatura.pack()
-
-# Valores de prueba
-valores_prueba = [50, 100, 150, 200, 250, 220, 190, 180, 170, 200, 150, 120, 95]
-
-# Mostrar los valores de prueba en forma gráfica
-for valor in valores_prueba:
-    actualizar_grafica(valor)
-    ventana.update()
-    plt.pause(1)  # Pausa de 1 segundo para simular la actualización en tiempo real
-
-ventana.mainloop()
+# Start the main loop
+root.mainloop()
